776f8162e8f0cfc0e3c74085ddc2678f3b58a3a9
miglael <michaelbimaputra@gmail.com>
2026-02-17 19:11:42 +0700

    fix(program.cs) = fix Jwt

--- Program.cs
+++ Program.cs
@@ -20,20 +20,23 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 .AddEntityFrameworkStores<AppDbContext>()
 .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication("JwtBearer")
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+})
 .AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
             Encoding.UTF8.GetBytes("THIS_IS_MY_SUPER_SECRET_BOOKING_CAMPUS_KEY_2026_SECURE_123"))
-
-        };
-    });
+    };
+});
 builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
